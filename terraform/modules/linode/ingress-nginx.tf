1ff97460d8f7050802fb982b48479c8d8d7759d1
megumish <megumish@exploitouri.st>
2022-01-29 16:19:10 +0900

    refactor(terraform): remove unnecessary module

--- terraform/modules/linode/ingress-nginx.tf
+++
@@ -1,7 +0,0 @@
-module "ingress_nginx" {
-  source = "../base-cluster/modules/ingress-nginx"
-
-  kube_host                   = module.lke.kube_host
-  kube_token                  = module.lke.kube_token
-  kube_cluster_ca_certificate = module.lke.kube_cluster_ca_certificate
-}
