6d493f5c397c0cf43f29c3d7ff77f489c3da3eeb
megumish <megumish@exploitouri.st>
2022-01-29 18:04:40 +0900

    feat(bootstrap|iam): add keycloak app in iam prj

---
+++ terraform/modules/base-cluster/modules/cert-manager/variables copy.tf
@@ -0,0 +1,23 @@
+variable "kube_host" {
+  type = string
+}
+
+variable "kube_token" {
+  type = string
+}
+
+variable "kube_cluster_ca_certificate" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "argocd_password" {
+  type = string
+}
+
+variable "domain" {
+  type = string
+}
