f30ccc89b9c0894e821f00dd82e325ed17fcc8ff
megumish <megumish@exploitouri.st>
2022-01-30 22:38:43 +0900

    change(terraform): linode storage class change to suffix retain

--- terraform/modules/linode/locals.tf
+++ terraform/modules/linode/locals.tf
@@ -3,5 +3,5 @@ locals {
   argocd_namespace             = "argo-cd"
   traefik_namespace            = "traefik"
   external_dns_namespace       = "external-dns"
-  cloud_provider_storage_class = "linode-block-storage"
+  cloud_provider_storage_class = "linode-block-storage-retain"
 }
