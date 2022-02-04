345c4181f16abad5deaf1d1182c40aa71a04282f
megumish <megumish@exploitouri.st>
2022-02-04 16:09:15 +0900

    fix(terraform): try to add destroy duration

--- terraform/modules/gcp/modules/gke/project-factory-sa.tf
+++ terraform/modules/gcp/modules/gke/project-factory-sa.tf
@@ -22,5 +22,6 @@ data "google_iam_policy" "project_factory_additional" {
 resource "time_sleep" "wait_for_applying_project_factory_additional" {
   depends_on = [google_project_iam_policy.project_factory_additional]
 
-  create_duration = "5m"
+  create_duration  = "5m"
+  destroy_duration = "5m"
 }
