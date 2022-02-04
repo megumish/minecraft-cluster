94fc870554b92ba99f3dca57b9b359d642350377
megumish <megumish@exploitouri.st>
2022-02-04 10:38:10 +0900

    fix(terraform): remove something that needs to connect to existing resources.

--- terraform/modules/gcp/modules/project/project.tf
+++ terraform/modules/gcp/modules/project/project.tf
@@ -7,8 +7,6 @@ module "project" {
   name              = var.project_name
   billing_account   = var.billing_account_id
   folder_id         = module.folder.id
-  group_name        = "project-editor"
-  group_role        = "roles/editor"
   create_project_sa = true
   project_sa_name   = "project-service-account"
   activate_apis = [
@@ -25,8 +23,6 @@ module "project" {
   labels                             = {}
   default_service_account            = "delete"
   budget_amount                      = var.budget_amount
-  vpc_service_control_attach_enabled = true
-  vpc_service_control_perimeter_name = "vscp-${var.project_name}"
   grant_services_security_admin_role = false
   grant_services_network_role        = false
 }
