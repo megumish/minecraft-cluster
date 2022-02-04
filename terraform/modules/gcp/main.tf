b7038208f991a6154d30668290815a376c9e9612
megumish <megumish@exploitouri.st>
2022-02-04 12:16:06 +0900

    feat(terraform): gke factory sa

--- terraform/modules/gcp/main.tf
+++ terraform/modules/gcp/main.tf
@@ -6,3 +6,9 @@ terraform {
     }
   }
 }
+
+resource "google_service_account" "service_account" {
+  project      = module.project.project_id
+  account_id   = "gke-factory"
+  display_name = "GKE Factory"
+}
