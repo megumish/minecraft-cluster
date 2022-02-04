0d6c5cd4b6f06b5d46150889d54033f71afb367b
megumish <megumish@exploitouri.st>
2022-02-04 19:27:43 +0900

    feat(terraform): delete gke network

--- terraform/modules/gcp/modules/gke/network.tf
+++ terraform/modules/gcp/modules/gke/network.tf
@@ -1,11 +1,12 @@
-resource "google_compute_network" "gke" {
-  provider = google.gke_factory
-
-  project                 = var.project_id
-  name                    = "gke"
-  auto_create_subnetworks = false
-
-  depends_on = [
-    google_project_iam_policy.gke_factory_and_project_factory_additional
-  ]
-}
+# resource "google_compute_network" "gke" {
+#   provider = google.gke_factory
+# 
+#   project                 = var.project_id
+#   name                    = "gke"
+#   auto_create_subnetworks = false
+# 
+#   depends_on = [
+#     google_project_iam_policy.gke_factory_and_project_factory_additional
+#   ]
+# }
+# 
