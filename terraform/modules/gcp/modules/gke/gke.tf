d65c7b3e8acc7a1234a430a32b5b846a4d73b4aa
megumish <megumish@exploitouri.st>
2022-02-06 15:09:37 +0900

    feat(terraform): add node

--- terraform/modules/gcp/modules/gke/gke.tf
+++ terraform/modules/gcp/modules/gke/gke.tf
@@ -19,6 +19,22 @@ module "gke" {
   node_pools = [
     {
       name = var.cluster_name
+    },
+    {
+      name               = "${random_id.node_name.keepers.cluster_name}-${random_id.node_name.hex}"
+      machine_type       = "e2-micro"
+      node_locations     = var.zone
+      min_count          = 1
+      max_count          = 1
+      local_ssd_count    = 0
+      disk_size_gb       = 10
+      disk_type          = "pd-standard"
+      image_type         = "COS_CONTAINERD"
+      auto_repair        = true
+      auto_upgrade       = true
+      service_account    = var.cluster_sa_email
+      preemptible        = true
+      initial_node_count = 1
     }
   ]
   enable_resource_consumption_export = false
@@ -32,4 +48,13 @@ module "gke" {
   }
   create_service_account = false
   service_account        = var.cluster_sa_email
+}
+
+resource "random_id" "node_name" {
+  keepers = {
+    # Generate a new id each time we switch to a new AMI id
+    cluster_name = var.cluster_name
+  }
+
+  byte_length = 8
 }
