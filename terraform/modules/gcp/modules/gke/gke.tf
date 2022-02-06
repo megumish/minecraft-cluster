a982992622936fcd52d9fbd0b7d6c9b63b388889
megumish <megumish@exploitouri.st>
2022-02-06 17:45:08 +0900

    feat(terraform): add stronger node pool.

--- terraform/modules/gcp/modules/gke/gke.tf
+++ terraform/modules/gcp/modules/gke/gke.tf
@@ -34,6 +34,23 @@ module "gke" {
       autoscaling        = true
       preemptible        = true
       initial_node_count = 1
+    },
+    {
+      name               = "minecraft-medium"
+      machine_type       = "e2-medium"
+      node_locations     = var.zone
+      min_count          = 1
+      max_count          = 1
+      local_ssd_count    = 0
+      disk_size_gb       = 10
+      disk_type          = "pd-standard"
+      image_type         = "COS_CONTAINERD"
+      service_account    = var.cluster_sa_email
+      auto_repair        = true
+      auto_upgrade       = true
+      autoscaling        = true
+      preemptible        = true
+      initial_node_count = 1
     }
   ]
   enable_resource_consumption_export = false
