b847b6c2be0d6631356f8de811c8f6d6a1c290ca
megumish <megumish@exploitouri.st>
2022-02-06 17:33:50 +0900

    feat(terraform): up max resources

--- terraform/modules/gcp/modules/gke/gke.tf
+++ terraform/modules/gcp/modules/gke/gke.tf
@@ -2,20 +2,21 @@ module "gke" {
   source  = "terraform-google-modules/kubernetes-engine/google"
   version = "19.0.0"
 
-  project_id                 = var.project_id
-  name                       = var.cluster_name
-  regional                   = false
-  zones                      = [var.zone]
-  network                    = module.gke_vpc.network_name
-  subnetwork                 = module.gke_vpc.subnets_names[0]
-  kubernetes_version         = "1.21.6-gke.1500"
-  release_channel            = "REGULAR"
-  horizontal_pod_autoscaling = false
-  http_load_balancing        = false
-  maintenance_start_time     = "14:00"
-  ip_range_pods              = module.gke_vpc.subnets_secondary_ranges[0][0].range_name
-  ip_range_services          = module.gke_vpc.subnets_secondary_ranges[0][1].range_name
-  remove_default_node_pool   = true
+  project_id                      = var.project_id
+  name                            = var.cluster_name
+  regional                        = false
+  zones                           = [var.zone]
+  network                         = module.gke_vpc.network_name
+  subnetwork                      = module.gke_vpc.subnets_names[0]
+  kubernetes_version              = "1.21.6-gke.1500"
+  release_channel                 = "REGULAR"
+  horizontal_pod_autoscaling      = true
+  enable_vertical_pod_autoscaling = true
+  http_load_balancing             = false
+  maintenance_start_time          = "14:00"
+  ip_range_pods                   = module.gke_vpc.subnets_secondary_ranges[0][0].range_name
+  ip_range_services               = module.gke_vpc.subnets_secondary_ranges[0][1].range_name
+  remove_default_node_pool        = true
   node_pools = [
     {
       name               = "minecraft"
@@ -30,6 +31,7 @@ module "gke" {
       service_account    = var.cluster_sa_email
       auto_repair        = true
       auto_upgrade       = true
+      autoscaling        = true
       preemptible        = true
       initial_node_count = 1
     }
@@ -38,9 +40,9 @@ module "gke" {
   cluster_autoscaling = {
     enabled       = true
     min_cpu_cores = 1
-    max_cpu_cores = 1
+    max_cpu_cores = 4
     min_memory_gb = 1
-    max_memory_gb = 1
+    max_memory_gb = 4
     gpu_resources = []
   }
   create_service_account = false
