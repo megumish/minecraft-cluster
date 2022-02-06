be5944683da1b620bb415125abe4c8df88bc91d8
megumish <megumish@exploitouri.st>
2022-02-06 15:51:35 +0900

    feat(terraform): remove networks

--- terraform/modules/gcp/modules/gke/network.tf
+++ terraform/modules/gcp/modules/gke/network.tf
@@ -1,7 +1,7 @@
-resource "google_compute_network" "gke" {
-  project                 = var.project_id
-  name                    = "gke"
-  auto_create_subnetworks = false
-
-  depends_on = [var.project_iam_etag]
-}
+# resource "google_compute_network" "gke" {
+#   project                 = var.project_id
+#   name                    = "gke"
+#   auto_create_subnetworks = false
+# 
+#   depends_on = [var.project_iam_etag]
+# }
