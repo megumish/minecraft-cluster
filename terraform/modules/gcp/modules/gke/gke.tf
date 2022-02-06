3375c5b000e8cfcb4a6db6d51c033f84eef32875
megumish <megumish@exploitouri.st>
2022-02-06 22:14:01 +0900

    feat(terraform): remove unnecessary nodepool

--- terraform/modules/gcp/modules/gke/gke.tf
+++ terraform/modules/gcp/modules/gke/gke.tf
@@ -2,45 +2,24 @@ module "gke" {
   source  = "terraform-google-modules/kubernetes-engine/google"
   version = "19.0.0"
 
-  project_id                      = var.project_id
-  name                            = var.cluster_name
-  regional                        = false
-  zones                           = [var.zone]
-  network                         = module.gke_vpc.network_name
-  subnetwork                      = module.gke_vpc.subnets_names[0]
-  kubernetes_version              = "1.21.6-gke.1500"
-  release_channel                 = "REGULAR"
-  horizontal_pod_autoscaling      = false
-  enable_vertical_pod_autoscaling = false
-  http_load_balancing             = false
-  logging_service                 = "none"
-  monitoring_service              = "none"
-  maintenance_start_time          = "14:00"
-  ip_range_pods                   = module.gke_vpc.subnets_secondary_ranges[0][0].range_name
-  ip_range_services               = module.gke_vpc.subnets_secondary_ranges[0][1].range_name
-  remove_default_node_pool        = true
-  node_pools = concat(
-    [
-      {
-        name               = "minecraft-medium"
-        machine_type       = "e2-medium"
-        node_locations     = var.zone
-        min_count          = 3
-        max_count          = 10
-        local_ssd_count    = 0
-        disk_size_gb       = 20
-        disk_type          = "pd-standard"
-        image_type         = "COS_CONTAINERD"
-        service_account    = var.cluster_sa_email
-        auto_repair        = true
-        auto_upgrade       = true
-        autoscaling        = true
-        preemptible        = true
-        initial_node_count = 3
-      },
-    ],
-    [for np in local.node_pools : merge(local.node_pool_default, np)]
-  )
+  project_id                         = var.project_id
+  name                               = var.cluster_name
+  regional                           = false
+  zones                              = [var.zone]
+  network                            = module.gke_vpc.network_name
+  subnetwork                         = module.gke_vpc.subnets_names[0]
+  kubernetes_version                 = "1.21.6-gke.1500"
+  release_channel                    = "REGULAR"
+  horizontal_pod_autoscaling         = false
+  enable_vertical_pod_autoscaling    = false
+  http_load_balancing                = false
+  logging_service                    = "none"
+  monitoring_service                 = "none"
+  maintenance_start_time             = "14:00"
+  ip_range_pods                      = module.gke_vpc.subnets_secondary_ranges[0][0].range_name
+  ip_range_services                  = module.gke_vpc.subnets_secondary_ranges[0][1].range_name
+  remove_default_node_pool           = true
+  node_pools                         = [for np in local.node_pools : merge(local.node_pool_default, np)]
   enable_resource_consumption_export = false
   cluster_autoscaling = {
     enabled       = false
